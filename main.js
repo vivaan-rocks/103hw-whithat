7070f1b8483c5b3f74364d5d8df1b9a4f9477935
vivaan-rocks <79306482+vivaan-rocks@users.noreply.github.com>
2021-05-03 11:56:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ Webcam.set({
     document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'"/>';
   });
   }
+
+  console.log('ml5 version:', ml5.version)
+
+  classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iaRLBdNPT/model.json',modelLoaded);
+
+  function modelLoaded(){
+    console.log('Model Loaded!');
+  }
+  function check(){
+    img=document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+  }
+
+  function gotResult(error, results){
+    if (error){
+      console.error(error);
+    } else{
+      console.log(results);
+      document.getElementById("object").innerHTML=results[0].label;
+      document.getElementById("accurracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+  }
