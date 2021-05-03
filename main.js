211c6ccf4bc96703f838ba43406de9cfc78c6031
vivaan-rocks <79306482+vivaan-rocks@users.noreply.github.com>
2021-05-03 11:22:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 Webcam.set({
-  width:350,
-  height:300,
-  image_format: 'png',
-  png_quality:90
-});
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+  });
+  
+  camera=document.getElementById("camera");
+  
+  Webcam.attach("#camera");
 
-camera=document.getElementById("camera");
-
-Webcam.attach("#camera")
+  function take_snapshot(){
+  Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'"/>';
+  });
+  }
